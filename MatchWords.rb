47c644beee9580d54281594cb77b98c3ec1fa28a
Wsl_F <WasylFranchuk@gmail.com>
2016-05-03 21:45:15 +0300

    add calculation probability of translation

--- MatchWords.rb
+++ MatchWords.rb
@@ -1,7 +1,45 @@
+#coding utf-8
+require 'unicode';
+
+class String
+  def last(n)
+    self[(self.length - n)..-1]
+  end
+
+  def downcase
+    Unicode::downcase(self)
+  end
+
+  def downcase!
+    self.replace downcase
+  end
+
+  def upcase
+    Unicode::upcase(self)
+  end
+
+  def upcase!
+    self.replace upcase
+  end
+
+  def capitalize
+    Unicode::capitalize(self)
+  end
+
+  def capitalize!
+    self.replace capitalize
+  end
+end
+
+
 class MatchTranslation
 
 
   def initialize(engSentence, rusSentence)
+    engSentence= engSentence.downcase
+    rusSentence= rusSentence.downcase
+
+
     @engSentence= engSentence.split(' ')
     @rusSentence= rusSentence.split(' ')
 
@@ -65,4 +103,109 @@ class MatchTranslation
   end
 
 
+  def levenshtein_distance(s, t)
+    m = s.length
+    n = t.length
+    return m if n == 0
+    return n if m == 0
+    d = Array.new(m+1) { Array.new(n+1) }
+
+    (0..m).each { |i| d[i][0] = i }
+    (0..n).each { |j| d[0][j] = j }
+    (1..n).each do |j|
+      (1..m).each do |i|
+        d[i][j] = if s[i-1] == t[j-1] # adjust index into string
+                    d[i-1][j-1] # no operation required
+                  else
+                    [d[i-1][j]+1, # deletion
+                     d[i][j-1]+1, # insertion
+                     d[i-1][j-1]+1, # substitution
+                    ].min
+                  end
+      end
+    end
+
+    return d[m][n]
+  end
+
+
+  def calcProbability(actualWord, translationWord)
+    d= levenshtein_distance(actualWord, translationWord)
+    #puts "distance: #{d}"
+    #puts "length: #{actualWord.size}"
+    #puts "p: #{d*1.0/actualWord.size}"
+
+    p= 0.0
+    if (d==0)
+      p= 1.0
+    else
+      p= 1.0 - d*1.0/actualWord.size
+    end
+
+    if (p<0)
+      p= 0.0
+    end
+
+    return p
+  end
+
+
+  def getProbability(engWord)
+    translations_= getTranslations(engWord)
+    translations= deleteEndings(translations_)
+
+    puts "translations without endings:"
+    puts translations.join(", ")
+    puts "\n"
+
+    probability= {}
+
+    @rusSentence.each { |actualWord|
+      probability[actualWord]= 0.0
+      translations.each do |transl|
+        p= calcProbability(actualWord, transl)
+        puts "p: #{p} actual word: #{actualWord}  translation: #{transl}"
+        if (probability[actualWord]<p)
+          probability[actualWord]= p
+        end
+      end
+    }
+
+
+    puts "\n\nprobability for translations of word \"#{engWord}\":\n"
+    probability.each do |key, value|
+      puts "#{key}:#{value}"
+    end
+
+    return probability
+  end
+
+
+  def getTranslations(engWord) # here call to Max's code
+    # this is stub method
+    case engWord
+      when 'street'
+        return ['улица', 'ул', 'стрит', 'дорожка', 'след', 'проход', 'коридор']
+      when 'people'
+        return ['нация', 'население', 'жители', 'родители', 'люди', 'родные', 'свита', 'команда', 'рабы', 'народ']
+      when 'are'
+        return ['ар', 'это', 'быть', 'являются', 'находиться', 'происходить', 'случаться']
+      when 'walking'
+        return ['ходьба', 'походка', 'хождение', 'гулянье', 'пробежка', 'движение', 'обход']
+    end
+
+    return []
+  end
+
 end
+
+
+myTranslation= MatchTranslation.new("People are walking on the street", "Люди гуляют на улице")
+puts "probability:"
+puts myTranslation.calcProbability("улиц", "умниц")
+puts myTranslation.calcProbability("бежать", "бегут")
+puts myTranslation.calcProbability("беж", "бег")
+
+
+puts "probability for all words"
+hash= myTranslation.getProbability("street").to_s
