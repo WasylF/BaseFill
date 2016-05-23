2f927b1de5463d179bc4fa15b701dfc1a0255f74
Wsl_F <WasylFranchuk@gmail.com>
2016-05-23 18:28:25 +0300

    refactor + add call to multitran API

--- match_words.rb
+++ match_words.rb
@@ -1,5 +1,8 @@
 #coding utf-8
 require 'unicode'
+require 'csv'
+require_relative 'multitran'
+require_relative 'file_parser'
 
 class String
   def last(n)
@@ -40,34 +43,38 @@ class MatchTranslation
   # eng_sentence - english sentence from Excel
   # rus_sentence - russian sentence from Excel
   def initialize(eng_sentence, rus_sentence)
-    eng_sentence.downcase!
-    rus_sentence.downcase!
+    eng_sentence.each { |word| word.downcase!.force_encoding(Encoding::UTF_8) }
+    rus_sentence.each { |word| word.downcase!.force_encoding(Encoding::UTF_8) }
+    @eng_sentence = eng_sentence
+    @rus_sentence = rus_sentence
+    # eng_sentence.downcase!
+    # rus_sentence.downcase!
 
-    eng_sentence.force_encoding(Encoding::UTF_8)
-    rus_sentence.force_encoding(Encoding::UTF_8)
+    # eng_sentence.force_encoding(Encoding::UTF_8)
+    # rus_sentence.force_encoding(Encoding::UTF_8)
 
-    @eng_sentence = eng_sentence.split(' ')
-    @rus_sentence = rus_sentence.split(' ')
+    # @eng_sentence = eng_sentence.split(' ')
+    # @rus_sentence = rus_sentence.split(' ')
 
     # TODO: add words
-    @useless_eng_words= %w(a the an on in by is are am were was be been have has had)
-    @useless_rus_words= %w(а б бы в во на)
+    @useless_eng_words = %w(a the an on in by is are am were was be been have has had)
+    @useless_rus_words = %w(а б бы в во на)
 
     @rus_endings = %w(am ax cm ex а ам ами ас ая е ев ей ем еми емя ею её и ие ий им ими ит ите их ишь ию ия иям иями иях м ми мя о ов ого ое ой ом ому ою оё у ум умя ут ух ую ют шь ю я ям ями ях ёй ёт ёте ёх ёшь)
 
     delete_useless!
 
-    @used_rus= Array.new(@rus_sentence.length, false)
-    @used_eng= Array.new(@eng_sentence.length, false)
+    @used_rus = Array.new(@rus_sentence.length, false)
+    @used_eng = Array.new(@eng_sentence.length, false)
 
     @translation_eng_to_rus = Array.new(@eng_sentence.length)
 
-    @rus_infinitives= get_infinitives(@rus_sentence)
+    @rus_infinitives = get_infinitives(@rus_sentence)
     @rus_sentence = delete_endings(@rus_sentence)
 
-    @all_translations= {}
+    @all_translations = {}
     @eng_sentence.each { |eng_word|
-      @all_translations[eng_word]= translate(eng_word)
+      @all_translations[eng_word] = translate(eng_word)
     }
   end
 
@@ -96,12 +103,12 @@ class MatchTranslation
     if word_size <= 3
       return word
     else
-      3.downto(1).each { |i|
+      3.downto(1).each do |i|
         ending = word.last(i)
         if @rus_endings.include?(ending)
           return word[0, word_size - i]
         end
-      }
+      end
     end
 
     word
@@ -157,21 +164,21 @@ class MatchTranslation
     p_best= -1
     rus_index= -1
     (0..rus_size).each { |i|
-      if !@used_rus[i]
+      unless @used_rus[i]
         sentence_word= @rus_sentence[i]
         translations.each do |translation|
           p = calc_probability(sentence_word, delete_ending(translation))
           if p > p_best
-            p_best= p
-            best= translation
-            rus_index= i
+            p_best = p
+            best = translation
+            rus_index = i
           end
         end
       end
     }
 
     if p_best > MIN_PROBABILITY
-      @used_rus[rus_index]= true
+      @used_rus[rus_index] = true
       return best
     end
     BAD_TRANSLATION
@@ -180,19 +187,7 @@ class MatchTranslation
 
   # returns translations of eng_word (from multitran)
   def translate(eng_word) # here call to Max's ending code
-    # this is stub method
-    case eng_word
-      when 'street'
-        return %w(улица ул стрит дорожка след проход коридор)
-      when 'people'
-        return %w(нациянаселение жители родители люди родные свита команда рабы народ)
-      when 'are'
-        return %w(ар это быть являются находиться происходить случаться)
-      when 'walking'
-        return %w(ходьба походка хождение гулянье пробежка движение обход гулять)
-      else
-        return []
-    end
+    Multitran.translate(eng_word).map { |t| t[0] }
   end
 
 
@@ -217,9 +212,8 @@ class MatchTranslation
       infinitives[i]= inf.split('|')
       i+= 1
     }
-    return infinitives
+    infinitives
   end
-
 
   # matches eng words to russian if one of translation is an infinitive of some russian word
   # result stored in @translation_eng_to_rus field
@@ -246,7 +240,7 @@ class MatchTranslation
 
     translation.force_encoding(Encoding::UTF_8)
     (0..rus_size).each { |i|
-      if !@used_rus[i]
+      unless @used_rus[i]
         if @rus_infinitives[i].include?(translation)
           @used_rus[i]= true
           return i
@@ -254,9 +248,8 @@ class MatchTranslation
       end
     }
 
-    return -1
+    -1
   end
-
 
   # try to match every english word from sentence to one russian word
   # return hash{key:"eng word", value: "russian infinitive (from multitran)"}
@@ -280,10 +273,14 @@ class MatchTranslation
 
     result
   end
-
 end
 
-
-my_translation = MatchTranslation.new("People are walking on the street", "Люди гуляют на улице")
-matches= my_translation.process_sentences
+#my_translation = MatchTranslation.new(%w(I gathered in this infographic 15 most practical advice for men who would like to always look perfectly This is a real man\'s guide to style Read remember and act),
+#                                      %w(собрал в этой инфографике 15 самых дельных советов для мужчин которые бы хотели всегда выглядеть на отлично Это настоящий гид по мужскому стилю Читайте запоминайте и действуйте))
+rus= %w(нужно использовать нескольких режимов)
+eng= %w(need to use multiple modes)
+my_translation = MatchTranslation.new(eng, rus)
+#words = FileParser.parse('file2.csv')
+#my_translation = MatchTranslation.new(words[1][0], words[1][1])
+matches = my_translation.process_sentences
 matches.each { |key, value| puts "eng_word: #{key}   -   rus_word: #{value}" }
